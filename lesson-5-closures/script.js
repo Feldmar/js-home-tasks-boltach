695c2b7a5d4013fe9d760f82bd6df812c42cc960
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-15 22:35:44 +0300

    add

---
+++ lesson-5-closures/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var spenderJohn = [];
+var tips = [];
+
+function kmp() {
+  spenderJohn.push({
+    rest1 : 124,
+    rest2 : 48,
+    rest3 : 268,
+    rest4 : 180,
+    rest5 : 42
+  });
+  console.log(spenderJohn);
+}
+kmp();
