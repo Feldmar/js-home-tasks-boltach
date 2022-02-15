45c98b4602ffc7e36bb88780f71e1905a11d057f
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-15 23:27:49 +0300

    re

--- lesson-5-closures/script.js
+++ lesson-5-closures/script.js
@@ -3,14 +3,24 @@
 var spenderJohn = [];
 var tips = [];
 
-function kmp() {
-  spenderJohn.push({
-    rest1 : 124,
-    rest2 : 48,
-    rest3 : 268,
-    rest4 : 180,
-    rest5 : 42
-  });
+
+  for(var i = 0; i <= 0; i++){
+    spenderJohn.push({
+        rest1 : 124,
+        rest2 : 48,
+        rest3 : 268,
+        rest4 : 180,
+        rest5 : 42
+      });
+  }
   console.log(spenderJohn);
-}
-kmp();
+
+  for(var i = 0; i <= 0; i++){
+    tips.push({
+       tips1 :  0.2,
+       tips2 : 0.1,
+       tips3 :0.15
+      });
+  }
+  console.log(tips);
+
