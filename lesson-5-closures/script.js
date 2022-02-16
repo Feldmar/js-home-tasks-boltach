0ffe2efc36c5a8c49a511765bef64b55f3ca04f4
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-17 00:26:37 +0300

    lesson 5 hw

--- lesson-5-closures/script.js
+++ lesson-5-closures/script.js
@@ -1,26 +1,65 @@
 'use strict';
 
-var spenderJohn = [];
-var tips = [];
+var john = {
+	fullName : 'John J',
+	bills : [124, 48, 268, 180, 42],
+	tips : [],
+	total : [],
+	calcTips: function(){
+		for(var i = 0; i < this.bills.length; i++) {
+			var percentage;
+			var bill = this.bills[i];
+			if(bill < 50) {
+				percentage = 0.2;
+			} else if (bill >= 50 && bill < 200) {
+				percentage = 0.15;
+			} else {
+				percentage = 0.1;
+			}	
+		this.tips[i] = bill * percentage;
+		this.total[i] = (bill * 2) * percentage;	
+        }	
+	}
+}
+john.calcTips();
 
+var mark = {
+	fillName : 'Mark F',	
+	bills : [77, 375, 110, 45],
+    tips : [],
+	total : [],
+	calcTips: function(){
+		for(var i = 0; i < this.bills.length; i++) {
+			var percentage;
+			var bill = this.bills[i];
+			if (bill < 100){
+				percentage = 0.2;
+			} else if (bill >= 100 && bill < 300) {
+				percentage = 0.1;
+			} else {
+				percentage = 0.25;
+			}
+			this.tips[i] = bill * percentage;
+			this.total[i] = (bill * 2) * percentage;
+		}
+	}
+}
 
-  for(var i = 0; i <= 0; i++){
-    spenderJohn.push({
-        rest1 : 124,
-        rest2 : 48,
-        rest3 : 268,
-        rest4 : 180,
-        rest5 : 42
-      });
-  }
-  console.log(spenderJohn);
+mark.calcTips();
 
-  for(var i = 0; i <= 0; i++){
-    tips.push({
-       tips1 :  0.2,
-       tips2 : 0.1,
-       tips3 :0.15
-      });
-  }
-  console.log(tips);
+function calcСt(tips) {
+	var sum = 0;
+	for (var i = 0; i < tips.length; i++) {
+		sum = sum + tips[i];
+	}
+	return sum / tips.length;
+}
 
+var j = calcСt(john.tips);
+var m = calcСt(mark.tips);
+
+if (j > m) {
+	console.log( 'Cемья' +'\u00A0' +john.fullName+ ' в среднем заплатила больше всего чаевых, а именно :' +'\u00A0' +j);
+} else if (j < m) {
+	console.log('Cемья' +'\u00A0' +mark.fullName+ ' в среднем заплатила больше всего чаевых, а именно :' +'\u00A0' +m);
+}
