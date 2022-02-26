f6f1072aa8ec59c5130d59f38c221df0e770a87b
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-27 00:28:09 +0300

    z

--- lesson-8-oop/script2.js
+++ lesson-8-oop/script2.js
@@ -1,11 +1,14 @@
 'use strict';
 
-function isPal(palindromStr) {
-  var cleaning = palindromStr.replace(/[^A-Z0-9]/ig, ''); 
-  var verify = cleaning.split('').reverse().join('');
-  return cleaning === verify;
-}
-console.log(isPal('А роза упала на лапу Азора'));
-console.log(isPal('zz7'));
-console.log(isPal('787'));
-console.log(isPal('Уж редко рукою окурок держу'));
+// function isPal(palindromStr) {
+//   var cleaning = palindromStr.replace(/[^A-Z0-9]/ig, ''); 
+//   var verify = cleaning.split('').reverse().join('');
+//   return cleaning === verify;
+// }
+// console.log(isPal('А роза упала на лапу Азора'));
+// console.log(isPal('zz7'));
+// console.log(isPal('787'));
+// console.log(isPal('Уж редко рукою окурок держу'));
+
+
+
