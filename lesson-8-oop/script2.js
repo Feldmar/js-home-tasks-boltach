53df74e84d2235e7a80debec9163a26458394fbc
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-27 01:43:34 +0300

    lesson-8

--- lesson-8-oop/script2.js
+++ lesson-8-oop/script2.js
@@ -11,4 +11,17 @@ console.log(isPal('787'));
 console.log(isPal('Уж редко рукою окурок держу'));
 
 
+function isPal(str) { 
+  return str == str.split('').reverse().join('');
+}
+
+String.prototype.al = function() { 
+  return this.replace(/[^a-zA-Z0-9]/ig, ''); 
+};
+
+console.log(isPal('ingirumimusnocteetconsumimurigni'));
+console.log(isPal('А роза упала на лапу Азора'));
+console.log(isPal('zz7'));
+console.log(isPal('787'));
+console.log(isPal('Уж редко рукою окурок держу'));
 
