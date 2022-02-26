308c183eb972ff0e1c64e577b519f72cf71e3b86
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-02-27 02:16:53 +0300

    lesson 8 kmp

--- lesson-8-oop/script2.js
+++ lesson-8-oop/script2.js
@@ -11,17 +11,17 @@ console.log(isPal('787'));
 console.log(isPal('Уж редко рукою окурок держу'));
 
 
-function isPal(str) { 
-  return str == str.split('').reverse().join('');
-}
 
-String.prototype.al = function() { 
-  return this.replace(/[^a-zA-Z0-9]/ig, ''); 
+
+String.prototype.isPal = function () { // проблема в пробелах и верхнем регистре
+  return this.replace(/[^A-Z0-9]/ig, ''); 
 };
-
+function isPal(str) { 
+  return str === str.split('').reverse().join('');
+}
 console.log(isPal('ingirumimusnocteetconsumimurigni'));
-console.log(isPal('А роза упала на лапу Азора'));
+console.log(isPal('АрозаупаланалапуАзора'));
 console.log(isPal('zz7'));
 console.log(isPal('787'));
-console.log(isPal('Уж редко рукою окурок держу'));
+              
 
--- lesson-8-oop/script3.js
+++ lesson-8-oop/script3.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function anClean(arr) {
+  var obj = {};
+  for (var i = 0; i < arr.length; i++) {
+    var sort = arr[i].replace(/[^A-Z0-9]/ig, '').split('').sort().join(''); 
+    obj[sort] = arr[i]; 
+  }
+  var result = [];
+  for (var key in obj)
+  result.push(obj[key]);
+  return result;
+}
+var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
+console.log(anClean(arr));
