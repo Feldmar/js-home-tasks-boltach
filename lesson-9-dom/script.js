7ef487e47d5fe427213006d8a7cf2034de34d160
Feldmar <89993722+Feldmar@users.noreply.github.com>
2022-03-04 16:42:42 +0300

    lesson-9-dom re

--- lesson-9-dom/script.js
+++ lesson-9-dom/script.js
@@ -25,7 +25,7 @@ class HashStorage {
   }  
 }
 
-var coctailsStorage = new HashStorage();
+var DrinkStorage = new HashStorage();
 
 var addButton = document.getElementById('add-coctail');
 addButton.onclick = function () {
@@ -33,29 +33,29 @@ addButton.onclick = function () {
   var isAlcohol = window.confirm('Напиток алкогольный?');
   isAlcohol ? isAlcohol = 'да' : isAlcohol = 'нет';
   var recipe = window.prompt('Поделитесь рецептом пожалуйста...');
-  coctailsStorage.add(names, {names, isAlcohol, recipe});
-  console.log('Coctail was added', coctailsStorage.getValue(names));
+  DrinkStorage.add(names, {names, isAlcohol, recipe});
+  console.log('Coctail was added', DrinkStorage.getValue(names));
 };
 
 var remButton = document.getElementById('delete-coctail');
 remButton.onclick = function () {
-  var r = prompt('Значение');
-  console.log(coctailsStorage.deleteValue([r]));
+  var r = prompt('Введите название напитка которое хотите удалить ...');
+  console.log(DrinkStorage.deleteValue([r]));
 };
 
 var getButton = document.getElementById('get-coctail');
 getButton.onclick = function () {
   var z = prompt('Введите название напитка котрый хотите найти ...');
   var getCoctails = document.querySelector('p');
-  getCoctails.innerHTML = 'Название напитка : ' +Object.values(coctailsStorage.getValue(z)).slice(0, 1) + '<br>\n' + 
-  'Алкогольный : ' +Object.values(coctailsStorage.getValue(z)).slice(1, 2) + '<br>\n' + 
-  'Рецепт : ' +Object.values(coctailsStorage.getValue(z)).slice(2, 3); //читерство, как есть
-  console.log(Object.values(coctailsStorage.getValue(z)));
+  getCoctails.innerHTML = 'Название напитка : ' +Object.values(DrinkStorage.getValue(z)).slice(0, 1) + '<br>\n' + 
+  'Алкогольный : ' +Object.values(DrinkStorage.getValue(z)).slice(1, 2) + '<br>\n' + 
+  'Рецепт : ' +Object.values(DrinkStorage.getValue(z)).slice(2, 3); //читерство, как есть
+  console.log(Object.values(DrinkStorage.getValue(z)));
 };
 
 var allButton = document.getElementById('all-coctails'); 
 allButton.onclick = function () {
   var allCoctails = document.querySelector('p');
-  allCoctails.innerHTML = 'Названия всех напитков : ' + coctailsStorage.getKeys() ;
-  console.log(coctailsStorage.getKeys());
+  allCoctails.innerHTML = 'Названия всех напитков : ' + DrinkStorage.getKeys() ;
+  console.log(DrinkStorage.getKeys());
 };
